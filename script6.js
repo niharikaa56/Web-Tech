24486c4ac907172dadd4cea6481729fe354cc6c5
Niharika <goswaminiharika5656@gmail.com>
2025-11-05 19:02:32 +0530

    new file add

---
+++ script6.js
@@ -0,0 +1,17 @@
+let random=Math.random()
+let a=prompt("enter first number")
+let b=prompt("enter second number")
+let c=prompt("enteropera")
+
+let obj={
+    "+":"-",
+    "*":"+-",
+    "-":"/",
+    "/":"**"
+}
+
+if(random>0.1){
+alert("the result is $eva")
+}else{
+    c=obj(c)
+}
---
+++ script7.js
@@ -0,0 +1,27 @@
+console.log("this is strings tutorial")
+let a="niha"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a.length)
+
+let fname="Niha"
+let lname="Rika"
+console.log("Her name is "+fname+lname+"")
+console.log(`her name is ${fname} and is ${lname}`)//TEMPLATE LITERALS 
+
+let b="radhika"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+console.log(b.replace("rad","77"))
+console.log(b.concat(a))
+console.log(b.trim())//remove white spaces
+
+console.log(b)
+console.log(b.indexOf("h"))
+console.log(b.endsWith("s"))
+console.log(b.startsWith("r"))
