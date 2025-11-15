5cc6b881fe1c284c97bb4dbf8ba2a9c03ae366f5
Niharika <goswaminiharika5656@gmail.com>
2025-11-15 09:49:21 +0530

    updated file

--- script13.js
+++ script13.js
@@ -1,1 +1,38 @@
+let arr=[1,2,5,6,7]
+// index 0,1,2,3,4
 
+// arr[0]=45 //arrys are mutable therefore value is changes at index [0]
+// console.log(arr,typeof arr)
+// console.log(arr.length)
+
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[3])
+
+// console.log(arr.toString())//converts arrys to string
+
+// console.log(arr.join(" and "))
+
+// console.log(arr.pop())
+
+// console.log(arr.push("niha"))
+// console.log(arr)
+
+// console.log(arr.shift())
+// console.log(arr.unshift("niharika"))
+
+// console.log(delete arr[4])
+
+let a1=[1,2,3]
+let a2=[4,5,6]
+let a3=[8,7,9]
+console.log(a1.concat(a2,a3))
+
+console.log(a3.sort())
+
+let num=[1,2,3,4,5]
+console.log(num.splice(1,4))
+console.log(num.splice(1,4,44,55))
+console.log(num)
+
+console.log(num.slice(0,1))
---
+++ script14.js
@@ -0,0 +1,7 @@
+let a=[1,93,5,6,88]
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element)
+// }
+
+a.forEach(value)
